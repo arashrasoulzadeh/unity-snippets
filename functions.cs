9813badd2a91d10e66c4a4353d713f228e3fba26
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2015-12-07 06:45:12 +0330

    first release

---
+++ functions.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+ 
+
+///<summary>
+///<para>Author : Arash Rasoulzadeh (arashrasoulzadeh@gmail.com)</para>
+///<para>Last Update : 7 DEC 2015</para>
+///</summary>
+
+public class functions : MonoBehaviour
+{
+	
+	///<summary>
+	///<para>Using this function you can calculate if two values are near enugh or not.</para>
+	///<para>eg. : check for x or y of a vector ...</para>
+	///</summary>
+	public static bool CloseEnoughForMe (double value1, double value2, double acceptableDifference)
+	{
+		return Math.Abs (value1 - value2) <= acceptableDifference; 
+	}
+
+	///<summary>
+	///<para>Using this function you can calculate if two values are near enugh or not.</para>
+	///<para>eg. : check for x or y of a vector ...</para>
+	///</summary>
+	public static bool CloseEnoughForMe (float value1, float value2, float acceptableDifference)
+	{
+		return Math.Abs (value1 - value2) <= acceptableDifference; 
+	}
+
+}
